86bad938630c81a3c9a67423c0955a27e7848915
cjayers <ayerscj@mail.uc.edu>
2024-02-14 11:40:34 -0500

    second attempt

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,41 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         //Declaration of Variables
-        String trash ="";
-        int temp1;
-        double temp2 = (temp1 * 9/5) + 32;
+        String trash = "";
+        double temp1 = 0.0;
+        double temp2 = 0.0;
 
 
-    do {
-         // Getting user input
-         System.out.print("Enter a temperature in Celsius: ");
+        do {
+            // Getting user input
+            System.out.print("Enter a temperature in Celsius: ");
 
-         //Check for valid input
-        if(in.hasNextInt());
+            //Check for valid input
+            if (in.hasNextDouble()) {
 
-            temp1 = in.nextInt(); // Ok to read as a number
-            in.nextLine(); // Clears buffer
-
-        // Bad input not  an int or within range
-        else
-
-             trash = in.nextLine(); // Read as a string
-             System.out.println("You said the temperature is " + temp1 + "C");
-             System.out.println("That is not valid input or out or range try again.");
+                temp1 = in.nextDouble(); // Ok to read as a number
+                in.nextLine(); // Clears buffer
 
 
-    }while (temp1<=100 && temp1>=32);
+            }
+            // Bad input not  an int or within range
+            else {
+
+                trash = in.nextLine(); // Read as a string
+                System.out.println("You said the temperature is " + temp1 + "C");
+                System.out.println("That is not valid input or out or range try again.");
+            }
 
 
-          System.out.println("The temperature in Farhenheit: " + temp2 + "F");
+        } while (temp1 >= 100 && temp1 <= 0);
 
 
+        temp2 = temp1 * 1.8 + 32;
+        System.out.println("The temperature in Fahrenheit: " + temp2 + "F");
 
-
-
-
+    }
 }
