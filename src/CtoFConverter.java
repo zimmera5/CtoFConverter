33674a4913cc9142cdda3b9960934044d0c24b56
cjayers <ayerscj@mail.uc.edu>
2024-02-18 16:03:49 -0500

    Last attempt

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -9,27 +9,33 @@ public class CtoFConverter
         String trash = "";
         double temp1 = 0.0;
         double temp2 = 0.0;
+        boolean done = true;
 
 
         do {
             // Getting user input
             System.out.print("Enter a temperature in Celsius: ");
 
-            //Check for valid input
-            if (in.hasNextDouble()) {
+              do {
 
-                temp1 = in.nextDouble(); // Ok to read as a number
-                in.nextLine(); // Clears buffer
+                  //Check for valid input
+                  if (in.hasNextDouble()) {
+
+                      temp1 = in.nextDouble(); // Ok to read as a number
+                      in.nextLine(); // Clears buffer
+                      done = true;
 
 
-            }
-            // Bad input not  an int or within range
-            else {
+                  }
+                  // Bad input not  an int or within range
+                  else {
 
-                trash = in.nextLine(); // Read as a string
-                System.out.println("You said the temperature is " + temp1 + "C");
-                System.out.println("That is not valid input or out or range try again.");
-            }
+                      trash = in.nextLine(); // Read as a string
+                      System.out.println("You said the temperature is " + temp1 + "C");
+                      System.out.println("That is not valid input or out or range try again.");
+                      done = false;
+                  }
+              }while(!done);
 
 
         } while (temp1 >= 100 && temp1 <= 0);
@@ -37,6 +43,7 @@ public class CtoFConverter
 
         temp2 = (temp1 * 1.8) + 32;
         System.out.println("The temperature in Fahrenheit: " + temp2 + "F");
+
 
     }
 }
