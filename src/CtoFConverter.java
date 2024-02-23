9c3685b3f20f4b11b0bc4f987779a12ac8ba893f
zimmera5 <107568145+zimmera5@users.noreply.github.com>
2024-02-23 07:42:43 -0500

    Update CtoFConverter.java
    Added some comments for your loop and sentinel value

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -11,52 +11,41 @@ public class CtoFConverter
         double temp2 = 0.0;
         boolean done = true;
 
-
         do
         {
             // Getting user input
             System.out.print("Enter a temperature in Celsius: ");
 
+            //Check for valid input
 
+            if (in.hasNextDouble()){
 
-                  //Check for valid input
+                temp1 = in.nextDouble(); // Ok to read as a number
+                in.nextLine(); // Clears buffer
+                //this is a good place to put your 'done' flag, allows an exit to the loop
+                //sometimes people just put 'break;' and dont rely on a flag to exit a loop
+                //flags vs breaks is a topic debated a lot, and the answer between them is it depends
+                //breaks are nice when you have lots of loops or your loop is small
+                //flags are better for larger loops because of readability/control
 
-                   if (in.hasNextDouble())
-                   {
-
-                       temp1 = in.nextDouble(); // Ok to read as a number
-                       in.nextLine(); // Clears buffer
-                   }
-
-
-
-
-        // Bad input not  an int or within range
+                //if you want to test for range, do it here instead
+                //if it falls within range, you can set done = true - - else sout that number is out of range - - your loop will begin again
+ 
+            }
+            // Bad input not  an int or within range
             else {
 
                 trash = in.nextLine(); // Read as a string
                 System.out.println("You said the temperature is " + trash + "C");
                 System.out.println("That is not valid input or out or range try again.");
             }
+        }while( (!(temp1 < 101)) || (temp1 < -1)); //I wouldn't worry too much about this being your sentinel, make done your sentinel
 
-
-
-
-        }while( (!(temp1 < 101)) || (temp1 < -1));
-
-
+        //}while(done == false);
+        //now you can just have a simple flag, instead of going through the range testing
 
         temp2 = (temp1 * 1.8) + 32;
         System.out.printf("The temperature in Fahrenheit: %.2f", + temp2);
-
-
-
-
-
-
-
-
-
 
 
     }
